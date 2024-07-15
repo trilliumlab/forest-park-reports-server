79b7724f73faeb3ed7e0d6a14b584de8c30dbdaa
Forest Park <forestpark@forestpark.cecs.pdx.edu>
2024-07-15 19:57:41 +0000

    Remove deployment scripts

--- logs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/su root
-
-journalctl -u traileyes-staging -f --output cat -n 100
--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/su root
-
-# Fetch latest server
-su forestpark -c "git pull"
-# Stop existing server
-systemctl stop traileyes-staging
-# Start server
-systemctl enable traileyes-staging
-
